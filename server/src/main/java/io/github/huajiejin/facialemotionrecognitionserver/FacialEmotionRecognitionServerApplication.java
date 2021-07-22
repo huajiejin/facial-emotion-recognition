ad266ecfc41a09e9bd8662544bf8d3ac06b20492
jinhuajie <jinhuajie@tal.com>
2021-07-22 19:06:49 +0800

    feat(server): initialize sever with springboot

---
+++ server/src/main/java/io/github/huajiejin/facialemotionrecognitionserver/FacialEmotionRecognitionServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.huajiejin.facialemotionrecognitionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialEmotionRecognitionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacialEmotionRecognitionServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/io/github/huajiejin/facialemotionrecognitionserver/FacialEmotionRecognitionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.huajiejin.facialemotionrecognitionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacialEmotionRecognitionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
